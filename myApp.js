abca8c63e617f6cd11f5dfe1bc40ebc49b22483e
ProtonGamma <86150117+ProtonGamma@users.noreply.github.com>
2021-06-19 21:26:07 +0200

    pierwszaproba

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
 
-
+app.get("/json", (req, res) => {
+    res.json(
+        ver response;
+        if (process.env.VAR_NAME === "allCaps") {
+        response = "Hello World".toUpperCase();
+      } else {
+        response = "Hello World";
+      });
+  });
 
 
 
