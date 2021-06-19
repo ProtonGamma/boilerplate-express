feee1db4cc52c605df2ef52814ec6c2617dc9c73
ProtonGamma <86150117+ProtonGamma@users.noreply.github.com>
2021-06-19 21:49:25 +0200

    czwarte

--- myApp.js
+++ myApp.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get("/json", (req, res) => {
-    res.json(
-        if (process.env.MESSAGE_STYLE === uppercase) {
+        if (process.env.MESSAGE_STYLE === "uppercase") {
+            res.json(
         {"message": "HELLO JSON";}
+        )
       } else {
+        res.json(
         {"message": "Hello json";}
-      });
+        )
+      };
   });
 
 
