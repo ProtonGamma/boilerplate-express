a06b942b9bb4046106dbb52a13b2915c169b4b72
ProtonGamma <86150117+ProtonGamma@users.noreply.github.com>
2021-06-19 12:21:24 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+
+});
+
+app.use("/public", 
+  express.static(__dirname + "/public"));
 
 
 
